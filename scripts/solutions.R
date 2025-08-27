3b14a37bf9d930ecca8e88d1e11056408df421c1
Guri Giaever <ggiaever@gmail.com>
2025-08-26 23:12:41 -0700

    solutiions

--- scripts/solutions.R
+++ scripts/solutions.R
@@ -54,93 +54,6 @@ wrap_in_quotes <- function(...) {
   # Return the lines wrapped in single quotes
   return(paste0("'", code_lines, "'", collapse = ",\n"))
 }
-#
-# # Define the solutions function for collapsible output
-# solutions <- function(code_expr, result_value) {
-#   output_format <- knitr::opts_knit$get("rmarkdown.pandoc.to")
-#
-#   if (output_format == "html") {
-#     # Convert expressions to strings and wrap in backticks for code formatting
-#     code_lines <- sapply(code_expr, deparse)  # Convert each expression to a string
-#     wrapped_code <- paste("`", code_lines, "`", collapse = "\n")  # Wrap each code line in backticks
-#     return(knitr::asis_output(paste0(
-#       '<details>\n',
-#       '<summary><i>Click here to see the code and the result</i></summary>\n',
-#       '<pre><code>', wrapped_code, '</code></pre>\n',
-#       '<strong>Result:</strong> ', result_value, '\n',
-#       '</details>\n'
-#     )))
-#   } else {
-#     return(invisible(NULL))  # No output for non-HTML formats
-#   }
-# }
-#
-# How It Works
-# Inputs:
-#
-# code_block: A string containing R code (multi-line code is supported).
-# execute: If TRUE, the function executes the code and captures the result.
-# result_label: Label to prepend to the displayed result (default: "Result:").
-# Execution:
-#
-# If execute = TRUE, the function runs the code using eval(parse(text = code_block)) in a new environment (new.env()), capturing errors and output.
-# Output:
-#
-# Generates collapsible HTML for the code block.
-# Includes the result (if executed).
-# HTML Output:
-
-# Code appears inside <details> tags, and results (if executed) are displayed below the code.
-# Example Usage
-# 1. Code Only (Display Without Execution)
-# r
-# Copy code
-# solutions("
-# res_tableKD <- readRDS('data/res_tableKD.RDS')
-# res_tableKD_tb <- res_tableKD %>%
-#   data.frame() %>% rownames_to_column(var = 'gene') %>%
-#   dplyr::filter(!is.na(log2FoldChange)) %>% as_tibble()
-# ", execute = FALSE)
-# 2. Code with Execution
-# r
-# Copy code
-# solutions("
-# res_tableKD <- readRDS('data/res_tableKD.RDS')
-# res_tableKD_tb <- res_tableKD %>%
-#   data.frame() %>% rownames_to_column(var = 'gene') %>%
-#   dplyr::filter(!is.na(log2FoldChange)) %>% as_tibble()
-#
-# foldchanges <- res_tableKD_tb$log2FoldChange
-# names(foldchanges) <- res_tableKD_tb$gene
-# foldchanges <- sort(foldchanges, decreasing = TRUE)
-# matx <- cbind(foldchanges, foldchanges)
-# scoreMat = compSCORE(matx, coln = 1, sig = 0.58)
-# kresp = runGORESP(fdrThresh = 0.2, scoreMat = scoreMat,
-#   bp_input = hGOBP.gmt, go_input = NULL, minSetSize = 20,
-#   maxSetSize = 300)
-#
-# kvis = visSetup(kresp$enrichInfo, kresp$edgeMat)
-# ", execute = TRUE)
-# What You’ll See in HTML Output
-# Collapsed State
-# plaintext
-# Copy code
-# Click here to see the code
-# Expanded State
-# r
-# Copy code
-# res_tableKD <- readRDS('data/res_tableKD.RDS')
-# res_tableKD_tb <- res_tableKD %>%
-#   data.frame() %>% rownames_to_column(var = 'gene') %>%
-#   dplyr::filter(!is.na(log2FoldChange)) %>% as_tibble()
-#
-# foldchanges <- res_tableKD_tb$log2FoldChange
-# names(foldchanges) <- res_tableKD_tb$gene
-# foldchanges <- sort(foldchanges, decreasing = TRUE)
-# matx <- cbind(foldchanges, foldchanges)
-# scoreMat = compSCORE(matx, coln = 1, sig = 0.58)
-# kresp = runGORESP(fdrThresh = 0.2, scoreMat = scoreMat,
-#   bp_input = hGOBP.gmt, go_input = NULL, minSetSize = 20,
 
 # solutions expressiion works to plot igraph outide code block
 solutions <- function(code_expr) {
@@ -189,45 +102,6 @@ solutions <- function(code_expr) {
 }
 
 
-#
-# wrap_in_quotes <- function(...) {
-#   code_lines <- sapply(substitute(list(...))[-1], deparse)  # Capture input expressions
-#   return(paste0("'", code_lines, "'", collapse = ",\n"))  # Wrap each line in quotes
-# }
-# # Function to wrap raw code lines in quotes
-# wrap_in_quotes <- function(...) {
-#   # Collect all lines into a vector
-#   code_lines <- c(...)
-#
-#   # Return the lines wrapped in single quotes
-#   return(paste0("'", code_lines, "'", collapse = ",\n"))
-# }
-
-
-
-# solutions <- function(solution_text) {
-#   output_format <- knitr::opts_knit$get("rmarkdown.pandoc.to")
-#
-#   if (output_format == "html") {
-#     # Escape angle brackets and replace newlines for HTML output
-#     solution_text <- gsub("<", "&lt;", solution_text)
-#     solution_text <- gsub(">", "&gt;", solution_text)
-#     solution_text <- gsub("\n", "<br>", solution_text)
-#
-#     # Wrap the solution in <details> for collapsible HTML
-#     knitr::asis_output(paste0(
-#       '<details>\n',
-#       '<summary><i>Click here to see the solution</i></summary>\n',
-#       '<pre><code>', solution_text, '</code></pre>\n',
-#       '</details>\n'
-#     ))
-#   } else {
-#     # For non-HTML formats (EPUB, PDF), hide the solution
-#     cat("")  # No output for non-HTML formats
-#   }
-# }
-
-#
 solution3 <- function(solution_text) {
   output_format <- knitr::opts_knit$get("rmarkdown.pandoc.to")
 
@@ -244,21 +118,3 @@ solution3 <- function(solution_text) {
     cat("")  # No output for non-HTML formats
   }
 }
-#
-# solutions <- function(solution_text) {
-#   output_format <- knitr::opts_knit$get("rmarkdown.pandoc.to")
-#
-# #  if (output_format == "html") {
-#     # For GitBook (HTML), output as raw HTML and wrap the solution in <pre><code> tags
-#     knitr::asis_output(paste0(
-#       '<details>\n',
-#       '<summary><i>Click here to see the solution</i></summary>\n',
-#       '<pre><code>', solution_text, '</code></pre>\n',
-#       '</details>\n'
-#     ))
-# #  } else {
-#     # For non-HTML formats (EPUB, PDF), hide the solution
-#     # cat("")  # No output for non-HTML formats
-#   }
-# }
-#
